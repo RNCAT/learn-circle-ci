b170c304afb7256a019c5263109852fc03a23582
Jaruwat Thongchana <jaruwat.works@gmail.com>
2023-01-17 00:36:17 +0700

    fix: edit response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 http
   .createServer((req, res) => {
-    res.write('Hello CircleCI')
+    res.write('Hello CircleCI edited')
     res.end()
   })
   .listen(3000, '0.0.0.0')
